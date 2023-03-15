a66de4e42a14cd9c5d161699ec2b14a9e2b6d13d
Thomas Helml <thomas@helml.com>
2023-03-15 14:20:55 +0100

    initial commit

---
+++ src/main/java/com/helml/ApplicationConfig.java
@@ -0,0 +1,8 @@
+package com.helml;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+//@ApplicationPath("api") // => API Path beginnt mit api
+public class ApplicationConfig extends Application {
+}
